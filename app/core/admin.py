5ea7a64b4a331ea7e1efbdfda18d251ee2a7a88b
kacabaj <kacabaj@pgs-soft.com>
2022-08-16 18:00:06 +0200

    API for creating users. Swagger

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from  django.utils.translation import  gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
@@ -10,6 +10,7 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
             {
@@ -23,5 +24,20 @@ class UserAdmin(BaseUserAdmin):
         (_('Important dates'), {'fields': ('last_login',)})
     )
     readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
+
 
 admin.site.register(models.User, UserAdmin)
